1fb92318ee9781e60d38c2dadac53e7db81f8258
pappakos0403 <pappakos0403@gmail.com>
2025-07-01 21:11:39 +0200

    sql + fastapi

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import sqlite3
 
+# FastAPI példány
 app = FastAPI()
+
+# Adatbázis kapcsolat
+conn = sqlite3.connect('tasks_database.db', check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS tasks (
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               name TEXT,
+               type TEXT,
+               is_done BOOLEAN
+               )
+''')
 
 # Feladat osztálya
 class Task(BaseModel):
+    id: int | None = None
     name: str = None
     type: str = None
     is_done: bool = False
-
-# Feladatok listája
-tasks = []
 
 @app.get("/")
 def root():
@@ -19,27 +32,78 @@ def root():
 # Feladat hozzáadása
 @app.post("/tasks")
 def create_task(task: Task):
-    tasks.append(task)
-    return tasks
+    # Adatok hozzáadása az adatbázishoz
+    cursor.execute(
+        "INSERT INTO tasks (name, type, is_done) VALUES (?, ?, ?)",
+        (task.name, task.type, int(task.is_done))
+    )
+    conn.commit()
+
+    # Visszajelzés küldése
+    new_id = cursor.lastrowid
+    cursor.execute(
+        "SELECT id, name, type, is_done FROM tasks WHERE id = ?",
+        (new_id,)
+    )
+    row = cursor.fetchone()
+    return {
+        "id": row[0],
+        "name": row[1],
+        "type": row[2],
+        "is_done": bool(row[3])
+    }
 
 # Feladatok listájának lekérése
 @app.get("/tasks")
 def list_tasks(limit: int = 10):
-    return tasks[0:limit]
+    cursor.execute(
+        "SELECT id, name, type, is_done FROM tasks LIMIT ?",
+        (limit,)
+    )
+    rows = cursor.fetchall()
+    tasks = []
+    for row in rows:
+        tasks.append({
+            "id": row[0],
+            "name": row[1],
+            "type": row[2],
+            "is_done": bool(row[3])
+        })
+    return tasks
 
 # Adott feladat lekérése
 @app.get("/tasks/{task_id}")
 def get_task(task_id: int) -> Task:
-    if task_id < len(tasks):
-        return tasks[task_id]
-    else:
-        raise HTTPException(status_code=404, detail=f"Nem található a következő feladat: {task_id}")
+    cursor.execute(
+        "SELECT id, name, type, is_done FROM tasks WHERE id = ?",
+        (task_id,)
+    )
+    row = cursor.fetchone()
+    if row is None:
+        raise HTTPException(status_code=404, detail=f"Nem található ez a feladat: {task_id}")
+    task = {
+        "id": row[0],
+        "name": row[1],
+        "type": row[2],
+        "is_done": bool(row[3])
+    }
+    return task
     
 # Adott feladat törlése
 @app.delete("/tasks/{task_id}")
-def delete_task(task_id: int) -> Task:
-    if task_id < len(tasks):
-        deleted_task = tasks.pop(task_id)
-        return deleted_task
-    else:
-        raise HTTPException(status_code=404, detail=f"Nem létezik {task_id} számú feladat!")
+def delete_task(task_id: int):
+    cursor.execute(
+        "SELECT id FROM tasks WHERE id = ?",
+        (task_id,)
+    )
+
+    row = cursor.fetchone()
+    if row is None:
+        raise HTTPException(status_code=404, detail=f"Nem található ez a feladat: {task_id}")
+    
+    cursor.execute(
+        "DELETE FROM tasks WHERE id = ?",
+        (task_id,)
+    )
+    conn.commit()
+    return {"message": f"{task_id} törölve!"}
